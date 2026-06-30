eb104fbdf7068b9c309b4fa24909c1af0ea0b621
rose979 <franz.rose@studmail.w-hs.de>
2026-06-30 17:50:03 +0200

    test aggressive normalization method

--- scripts/01_load_data.R
+++ scripts/01_load_data.R
@@ -2,4 +2,6 @@
 # Initial script to load Chlorella transcriptomic count matrices
 
 print("Hello Chlorella! Ready for analysis.")
-print("We are starting with a snakemake workflow.")
+
+# EXPERIMENT: Trying out a new aggressive normalization method
+print("Running experimental normalization... hope it doesn't break!")

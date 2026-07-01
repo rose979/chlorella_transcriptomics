1cfe3c912f240e2a5ab97f1151c5ddf0552fc752
rose979 <franz.rose@studmail.w-hs.de>
2026-07-01 11:42:26 +0200

    adding description of planned changes

--- scripts/03_conf_analysis.R
+++ scripts/03_conf_analysis.R
@@ -13,6 +13,9 @@ print(paste("Using min_count =", min_count, ", min_samples =", min_samples,
 # set DESeq analysis parameters for differential expression analysis
 design_formula <- ~ ammonium # Replicates are irrelevant so not included
 
+
+########################################################################################
+# maybe write a function, that selects the configuration of deseq out of the listed cases?
 # Contrasts for differential expression
 contrasts_list <- list("treatment_vs_control" = c("condition", "treatment", "control"))
  
@@ -21,3 +24,4 @@ contrasts_list <- list("ammonium" = "ammonium")  # Contrast for ammonium concent
 
 # If you code ammonium as 0 and 600, the coefficient is “per 1 unit ammonium,” so the beta will look ~0 (because the real effect is spread over 600 units). That’s why the log2FCs look tiny.
 contrasts_list <- list("ammonium" = c("ammonium", "600", "0"))
+########################################################################################
